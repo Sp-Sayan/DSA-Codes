8c38fd3544322ad653fde2a2590b5accd558434b
Sayan Paul <131338948+Sp-Sayan@users.noreply.github.com>
2025-02-23 18:27:00 +0530

    Create OddAndEvenLL.java

---
+++ Linked List/OddAndEvenLL.java
@@ -0,0 +1,40 @@
+/*
+https://leetcode.com/problems/odd-even-linked-list
+ */
+package sayan_LL;
+
+public class OddAndEvenLL {
+    public ListNode oddEvenList(ListNode head) {
+
+        if(head == null || head.next == null)
+            return head;
+        ListNode insertPoint = head;
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while(fast != null && fast.next != null){
+
+            //swap
+            slow.next = fast.next;
+            //ListNode temp = fast.next;
+            fast.next = insertPoint.next;
+            insertPoint.next = fast;
+            insertPoint = fast;
+            fast = slow.next.next;
+
+            slow = slow.next;
+
+        }
+
+        //for last node of odd no. of nodes
+        if(fast != null){
+            //swap
+            slow.next = fast.next;
+            //ListNode temp = fast.next;
+            fast.next = insertPoint.next;
+            insertPoint.next = fast;
+            insertPoint = fast;
+        }
+
+        return head;
+    }
+}
