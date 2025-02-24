7a333b911885d6c9e0a9c87d5e1e250e347027f5
Sayan Paul <131338948+Sp-Sayan@users.noreply.github.com>
2025-02-24 20:43:16 +0530

    Create PivotIndex.java

---
+++ Arrays & String/PivotIndex.java
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/find-pivot-index/
+ */
+
+package com.sayan;
+
+public class PivotIndex {
+    public int pivotIndex(int[] nums) {
+
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        //find sum of all elements
+        for(int i = 0;i<nums.length;i++)
+            rightSum += nums[i];
+
+        //searching for pivot
+        for(int i = 0; i< nums.length;i++){
+
+            rightSum -= nums[i];        //we subtract the current element from rightsum to get the sum of right elements
+
+            if(leftSum == rightSum)
+                return i;
+
+            leftSum += nums[i];         //we add the current element from leftsum to get the sum of left elements
+
+        }
+
+        return -1;
+    }
+}
