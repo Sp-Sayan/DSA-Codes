208bfe39e58a0017768e109b2d6824dd96ed990a
Sayan Paul <131338948+Sp-Sayan@users.noreply.github.com>
2025-06-14 21:16:27 +0530

    Added MaxDiffAfterRemap

---
+++ Alpha_5.0/Basics & Maths/MaxDiffAfterRemap.java
@@ -0,0 +1,61 @@
+/*
+ * https://leetcode.com/problems/maximum-difference-by-remapping-a-digit/description/
+ */
+
+public class MaxDiffAfterRemap {
+
+    public int minMaxDifference(int num) {
+        int min = 0;
+        int max = 0;
+        char remap = ' ';
+        Integer n1 = num;
+
+        String n2 = n1.toString();
+
+        // find first non-nine digit for max
+        for (int i = 0; i < n2.length(); i++) {
+            char x = n2.charAt(i);
+
+            if (x != '9') {
+                remap = x;
+                break;
+            }
+
+        }
+
+        // find max
+        for (int i = 0; i < n2.length(); i++) {
+            char x = n2.charAt(i);
+            int digit = Integer.parseInt(Character.toString(x));
+
+            if (x == remap)
+                digit = 9;
+
+            max = (max * 10) + digit;
+        }
+
+        // find first non zero digit for min
+        for (int i = 0; i < n2.length(); i++) {
+            char x = n2.charAt(i);
+
+            if (Integer.parseInt(Character.toString(x)) > 0) {
+                remap = x;
+                break;
+            }
+
+        }
+
+        // find min
+        for (int i = 0; i < n2.length(); i++) {
+            char x = n2.charAt(i);
+            int digit = Integer.parseInt(Character.toString(x));
+
+            if (x == remap)
+                digit = 0;
+
+            min = (min * 10) + digit;
+        }
+
+        return max - min;
+    }
+}
